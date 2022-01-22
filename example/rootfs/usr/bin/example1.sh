b51457543e4a9c91c736930adddcec79afb63bdd
Nate Clark <Nathanlclark@gmail.com>
2022-01-22 13:19:47 -0500

    Pulled in pre-built mochad

--- example/rootfs/usr/bin/example1.sh
+++ example/rootfs/usr/bin/example1.sh
@@ -6,15 +6,16 @@
 main() {
     bashio::log.info "Starting MochaD"
 
-    mkdir /build
-    cd /build || exit
-    wget -O mochad.tgz http://sourceforge.net/projects/mochad/files/latest/download
-    tar -xzvf mochad*.t*
-    cd mochad* && ./configure
-    make
-    make install
+#    mkdir /build
+#    cd /build || exit
+#    wget -O mochad.tgz http://sourceforge.net/projects/mochad/files/latest/download
+#    tar -xzvf mochad*.t*
+#    cd mochad* && ./configure
+#    make
+#    make install
     #apk --purge del gcc g++ cmake make wget
     #rm -rf /var/cache/apk/* /lib/apk/db/*
+    cd mochad
     mochad -d
 
     bashio::log.info "Done starting MochaD"
